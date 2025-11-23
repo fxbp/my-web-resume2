408abc0b41d3598f692ea7641b2cbe26d7918dba
Francesc Xavier Bullich Parra <u1939691@campus.udg.edu>
2025-11-23 21:30:56 +0100

    Update Resume.tsx
    Changed order of the main elements

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -8,28 +8,30 @@ import Skills from '../components/Skills';
 function Resume() {
     return (
         <>
-            <div  className='contentSection'>
-                <div className='contentTitle' >
-                    <h2><span className=''>My Skills</span></h2>
-                </div>
-                <Box flexGrow={1}>
-                    <Skills />
-                </Box>
-               
-            </div>
+            
             <div  className='contentSection'>
                 <div className='contentTitle' >
                     <h2><span>Resume</span></h2>
                 </div>
                 <Box flexGrow={1}>
                     <Grid container >
-                        <Grid item className="">
-                            <WorkTimeline />   
-                        </Grid> 
+                        
                         <Grid item className="">
                             <EducationTimeline />   
                         </Grid> 
+                        <Grid item className="">
+                            <WorkTimeline />   
+                        </Grid> 
                     </Grid>
+                </Box>
+               
+            </div>
+            <div  className='contentSection'>
+                <div className='contentTitle' >
+                    <h2><span className=''>My Skills</span></h2>
+                </div>
+                <Box flexGrow={1}>
+                    <Skills />
                 </Box>
                
             </div>
